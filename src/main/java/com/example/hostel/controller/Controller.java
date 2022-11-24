655e46b08747b2f1ddcf120869576d2a00ba8f1d
Shweta <Shweta@code>
2022-11-24 17:52:22 +0530

    Put and delete mapping Added

--- src/main/java/com/example/hostel/controller/Controller.java
+++ src/main/java/com/example/hostel/controller/Controller.java
@@ -3,23 +3,37 @@ package com.example.hostel.controller;
 import com.example.hostel.entity.StudentInfo;
 import com.example.hostel.service.ServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
+
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
+
+@RequestMapping("/api")
 public class Controller {
+
     @Autowired
     public ServiceImpl service;
-    @GetMapping("/Student")
+    @GetMapping("/student")
     private List<StudentInfo> getStudents(){
         return this.service.getStudents();
     }
-    @PostMapping("/studentinfo/{studentId}")
+    @PostMapping("/studentInfo/{studentId}")
     public StudentInfo getStudents(@PathVariable int studentId){
         return this.service.getStudents(studentId);
     }
+
+    @PutMapping("/student/{StudentId}")
+    public StudentInfo updateStudent(@PathVariable (value="StudentId")int StudentId, @RequestBody StudentInfo student ) {
+        return this.service.updateStudent(StudentId, student);
+    }
+
+    @DeleteMapping("/student/{StudentId}")
+    public StudentInfo deleteStudent(@PathVariable  String StudentId) {
+        return this.service.deleteStudent(Integer.parseInt(StudentId));
+    }
+
 }
