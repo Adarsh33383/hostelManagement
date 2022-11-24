34b8f771f9c8de52ffd7997c88b0484ed2d54f78
Shweta <Shweta@code>
2022-11-24 17:51:35 +0530

    Update and delete methods Added

--- src/main/java/com/example/hostel/service/Service.java
+++ src/main/java/com/example/hostel/service/Service.java
@@ -29,5 +29,35 @@ public class Service implements ServiceImpl {
         }
         return stud;
     }
+
+
+
+    @Override
+    public StudentInfo deleteStudent(int StudentId) {
+        StudentInfo st = null;
+        for (StudentInfo student : studentList) {
+            if (student.getStudentId() == StudentId) {
+                st = student ;
+                studentList.remove(student);
+                break;
+            }
+        }
+        return st;
+    }
+
+
+    public StudentInfo updateStudent(int StudentId, StudentInfo student) {
+        StudentInfo s = null;
+        for (StudentInfo student1 : studentList) {
+            if (student1.getStudentId() == StudentId) {
+                student1.setStudentName(student.getStudentName());
+                student1.setRoomNo(student.getRoomNo());
+                s = student1;
+                break;
+            }
+        }
+        return s;
+    }
+
 }
 
